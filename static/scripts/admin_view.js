6fe2df04e2e4f4a82815cdde92e0738b5ae18ffb
Javed Nissar <javednissar@gmail.com>
2018-12-02 16:48:35 -0500

    added modals for task creation and user invites

--- static/scripts/admin_view.js
+++ static/scripts/admin_view.js
@@ -13,6 +13,7 @@
  *      assignee: {
  *          id: int,
  *          name: str,
+ *          email:str
  *      },
  *  }],
  *  tasks:[{
@@ -25,6 +26,7 @@
  *      assignee: {
  *          id: int,
  *          name: str,
+ *          email: str
  *      },
  *  }],
  *  blockedTasks:[{
@@ -36,11 +38,13 @@
  *      assignee: {
  *          id: int,
  *          name: str,
+ *          email: str
  *      },
  *  }],
  *  users:[{
  *      id: int,
  *      name: str,
+ *      email: str
  *  }]
  * }]
  */
@@ -48,15 +52,16 @@
 /*
  * What follows is code for creating the pre-loaded data, this is where we would usually load from the server
  */
-function createUserObject(id, name){
+function createUserObject(id, name, email){
     return {
         id:id,
         name:name,
+        email:email
     }
 }
 
-const john = createUserObject(1,'John')
-const dave = createUserObject(2,'Dave')
+const john = createUserObject(1,'John',"johngreen@gmail.com")
+const dave = createUserObject(2,'Dave',"davegreen@gmail.com")
 
 function createTaskObject(id, text, blocked, done, priority, dueDate, assignee){
     return {
@@ -199,13 +204,66 @@ const taskComponent = Vue.component('task',{
     }
 });
 
+const validateEmail = (email) => {
+    const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+    return expression.test(String(email).toLowerCase())
+}
+
+const incrementMaxId = function(elementList){
+    const ids = elementList.map((element) => element.id);
+    const maxId = ids.reduce((acc,id) => {
+        return Math.max(acc, id);
+    });
+    return maxId + 1;
+}
+
+const foundUserFunction = function(emailToFind){
+    return function(user){
+        return user.email === emailToFind;
+    }
+}
 const app = new Vue({
     el: '#app',
     data: {
-        projects:[project,second_project]
+        projects:[project,second_project],
+        inviteUserEmail:"",
+        newTaskText:"",
+        newTaskUserEmail:"",
+        newTaskPriority:0,
+        newDueDate:new Date(),
+        inviteUserName:""
     },
     components:{
         task: taskComponent
+    },
+    methods:{
+        inviteUser:function(project){
+            const foundUser = project.users.find(foundUserFunction(this.inviteUserEmail));
+            if(validateEmail(this.inviteUserEmail) && foundUser === undefined){
+                const newUserId = incrementMaxId(project.users);
+                const newUser = createUserObject(newUserId, this.inviteUserName, this.inviteUserEmail);
+                project.users.push(newUser);
+                alert(`Email provided:${this.inviteUserEmail}`);
+            }else if(!validateEmail(this.inviteUserEmail)){
+                alert('Email is invalid');
+            }else{
+                alert('Duplicate user found');
+            }
+        },
+        createTask: function(project){
+            today = new Date();
+            inputDueDate = new Date(this.newDueDate);
+            if(today <= inputDueDate){
+                alert(`New Task Made With:(Name:${this.newTaskText},Due Date:${this.newDueDate.toString()},Assignee:${this.newTaskUserEmail}),Priority:${this.newTaskPriority}`);
+                const newId = incrementMaxId(project.tasks);
+                const foundUser = project.users.find(foundUserFunction(this.newTaskUserEmail));
+                const task = createTaskObject(newId,this.newTaskText,false, false, this.newTaskPriority, inputDueDate, foundUser);
+                project.tasks.push(task);
+            }else{
+                alert('Invalid due date');
+            }
+        }
     }
 });
 
